750070fe42b8f9ef7424e92fd34bca3e901f4f1d
Jas <webmaster@jaspaul.com>
2026-04-20 20:55:40 +0100

    ferry overlay

--- src/components/leaflet/LeafletMap.tsx
+++ src/components/leaflet/LeafletMap.tsx
@@ -1,9 +1,9 @@
 import { ferryRoutes } from '@/const'
 import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+import 'leaflet/dist/leaflet.css'
 import { LayerGroup } from 'react-leaflet/LayerGroup'
 import { LayersControl } from 'react-leaflet/LayersControl'
 import { MapContainer } from 'react-leaflet/MapContainer'
@@ -14,18 +14,6 @@ import { TileLayer } from 'react-leaflet/TileLayer'
 import styles from './LeafletMap.module.css'
 
 const { BaseLayer, Overlay } = LayersControl
-
-/** Bundled apps do not serve Leaflet's default relative icon URLs; wire real asset URLs once. */
-function bundledAssetUrl(mod: string | { src: string }) {
-  return typeof mod === 'string' ? mod : mod.src
-}
-
-Reflect.deleteProperty(L.Icon.Default.prototype, '_getIconUrl')
-L.Icon.Default.mergeOptions({
-  iconUrl: bundledAssetUrl(markerIcon),
-  iconRetinaUrl: bundledAssetUrl(markerIcon2x),
-  shadowUrl: bundledAssetUrl(markerShadow),
-})
 
 export default function LeafletMap() {
   return (
@@ -50,19 +38,36 @@ export default function LeafletMap() {
             />
           </BaseLayer>
 
-          <Overlay checked name={ferryRoutes[0].name}>
-            <LayerGroup>
-              <Polyline positions={ferryRoutes[0].path} />
-              <Marker position={ferryRoutes[0].from.position}>
-                <Popup>{ferryRoutes[0].from.name}</Popup>
-              </Marker>
-              <Marker position={ferryRoutes[0].to.position}>
-                <Popup>{ferryRoutes[0].to.name}</Popup>
-              </Marker>
-            </LayerGroup>
-          </Overlay>
+          <FerryRouteOverlay />
         </LayersControl>
       </MapContainer>
     </div>
+  )
+}
+
+const FerryRouteOverlay = () => {
+  /** Bundled apps do not serve Leaflet's default relative icon URLs; wire real asset URLs once. */
+  function bundledAssetUrl(mod: string | { src: string }) {
+    return typeof mod === 'string' ? mod : mod.src
+  }
+
+  Reflect.deleteProperty(L.Icon.Default.prototype, '_getIconUrl')
+  L.Icon.Default.mergeOptions({
+    iconUrl: bundledAssetUrl(markerIcon),
+    iconRetinaUrl: bundledAssetUrl(markerIcon2x),
+    shadowUrl: bundledAssetUrl(markerShadow),
+  })
+  return (
+    <Overlay checked name={ferryRoutes[0].name}>
+      <LayerGroup>
+        <Polyline positions={ferryRoutes[0].path} />
+        <Marker position={ferryRoutes[0].from.position}>
+          <Popup>{ferryRoutes[0].from.name}</Popup>
+        </Marker>
+        <Marker position={ferryRoutes[0].to.position}>
+          <Popup>{ferryRoutes[0].to.name}</Popup>
+        </Marker>
+      </LayerGroup>
+    </Overlay>
   )
 }
