f2fe3a2565b1b510b802fc05cf7b47d13cde8cd1
Jas <webmaster@jaspaul.com>
2026-04-21 12:35:07 +0100

    content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Map comparison</h1>
+      <ul>
+        <li>
+          <Link href="/leaflet">Leaflet</Link>
+        </li>
+        <li>
+          <Link href="/maplibre">MapLibre</Link>
+        </li>
+      </ul>
     </main>
   )
 }
