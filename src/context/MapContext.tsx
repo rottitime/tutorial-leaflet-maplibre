163a9c3f0377fc6fc779fb1d37f711e9ea42fe95
Jas <webmaster@jaspaul.com>
2026-04-30 12:05:54 +0100

    usehook with error fallback

--- src/context/MapContext.tsx
+++ src/context/MapContext.tsx
@@ -1,9 +1,17 @@
 'use client'
 import { Map } from 'maplibre-gl'
-import { createContext } from 'react'
+import { createContext, useContext } from 'react'
 
 const MapContext = createContext<Map | undefined>(undefined)
 
 export const MapProvider = ({ children }: { children: React.ReactNode }) => {
   return <MapContext.Provider value={undefined}>{children}</MapContext.Provider>
+}
+
+export const useMap = () => {
+  const context = useContext(MapContext)
+
+  if (!context) throw new Error('useMap must be used within a MapProvider')
+
+  return context
 }
